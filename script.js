dff4a9f624ee1dc217133f82055c85314f89e912
廣川　遥 <>
2020-07-05 11:28:04 +0900

    サイドバー 背景グレーがけ 実装できず

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(function() {
   // 「#js_sideMenuButton」のclickイベント→「#sidebar」を開く
   $('#js_sideMenuButton').click(function() {
-    $('#js_sidebar').addClass('is_open');
-    // ？　$('#sidebar_background').addClass('is_open');　？
+    $('#sidebar').addClass('is-open');
+    $('#sidebar__background').fadeIn();
     // ？　$('body').addClass('is_open');　？
   });
 
   // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
   $('#js_close_button').click(function() {
-      $('#js_sidebar').removeClass('is_open');
-      // ？　$('#sidebar_background').removeClass('is_open');　？
+      $('#sidebar').removeClass('is-open');
+      $('#sidebar__background').fadeOut();
       // ？　$('body').removeClass('is_open');　？
   });
   
