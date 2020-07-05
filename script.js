559668aaf14f528e6d0f9fcf8dbf50d8d38caddb
廣川　遥 <>
2020-07-05 23:45:38 +0900

    サイドバー グレーがけ 実装

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 $(function() {
   // 「#js_sideMenuButton」のclickイベント→「#sidebar」を開く
-  $('#js_sideMenuButton').click(function() {
+  $('#sidebar-button--open').click(function() {
     $('#sidebar').addClass('is-open');
     $('#sidebar__background').fadeIn();
-    // ？　$('body').addClass('is_open');　？
   });
 
   // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
   $('#js_close_button').click(function() {
       $('#sidebar').removeClass('is-open');
       $('#sidebar__background').fadeOut();
-      // ？　$('body').removeClass('is_open');　？
   });
   
+
+  var $win = $(window);
+
+  $win.on('load resize', function() {
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth >= 769) {
+      // PCの処理
+      $('#sidebar').removeClass('is-open');
+      $('#sidebar__background').fadeOut();
+    } 
+  });
 });
