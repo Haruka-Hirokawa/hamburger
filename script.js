8af736c8613f520a461f7e80bbbea14d2b9c8fd1
廣川　遥 <>
2020-07-09 10:24:17 +0900

    サイドバー　FLOCSSに従い編集

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(function() {
-  // 「#js_sideMenuButton」のclickイベント→「#sidebar」を開く
+  // 「#sidebar-button--open」のclickイベント→「#sidebar」を開く
   $('#sidebar-button--open').click(function() {
     $('#sidebar').addClass('is-open');
-    $('#sidebar__background').fadeIn();
+    $('#sidebar-background').fadeIn();
   });
 
   // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
-  $('#js_close_button').click(function() {
+  $('#sidebar-button--close').click(function() {
       $('#sidebar').removeClass('is-open');
-      $('#sidebar__background').fadeOut();
+      $('#sidebar-background').fadeOut();
   });
   
-
+// 画面サイズがPC以上になったらサイドバーを開いた状態でもPC用のデフォルト画面に戻す
   var $win = $(window);
 
   $win.on('load resize', function() {
@@ -20,7 +20,7 @@ $(function() {
     if (windowWidth >= 769) {
       // PCの処理
       $('#sidebar').removeClass('is-open');
-      $('#sidebar__background').fadeOut();
+      $('#sidebar-background').fadeOut();
     } 
   });
 });
